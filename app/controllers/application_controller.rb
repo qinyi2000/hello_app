c4d815537d040b06131e40f53aa99dbb7d7c2e98
Yi Qin <qinyi@grinnell.edu>
2019-02-06 03:37:43 +0000

    Update Gemfile for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # defining a controller means defining a method in the "Application Controller"
   # class. ApplicationController is some class that exists in the 
   # package from Rails framework
+  
+  # This is direct rendering, without a view. We are just
+  # returning raw html text to the browser
   def hello
     render inline: '<h1>Hello, World!</h1> 
-    <h2>¡Hola, mundo!</h2>
-    <em>Greetings by Yi</em>'
+    <h2>¡Hola, mundo!</h2>'
+  end
+  
+  def goodbye
+    render inline: '<h1>Goodbye</h1> 
+     <h2>Have fun coding!</h2>'
   end
 end
