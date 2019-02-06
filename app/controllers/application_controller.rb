551c5bdf45b549f26ebabde87f71a0154e772226
Yi Qin <qinyi@grinnell.edu>
2019-02-06 02:59:05 +0000

    hello_world made

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  # defining a controller means defining a method in the "Application Controller"
+  # class. ApplicationController is some class that exists in the 
+  # package from Rails framework
+  def hello
+    render html: "hello, world!"
+  end
+end
